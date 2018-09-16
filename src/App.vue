86871219c24fb6770d1ea0fa1d74239808925ab9
Arnelle Balane <arnellebalane@gmail.com>
2018-09-16 14:10:35 +0800

    Use ProximaNova for body fonts

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ html {
 
 body {
   margin: 0;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Proxima Nova", "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #fff;
