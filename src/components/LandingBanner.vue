a0b8692e5ad1bccc9d9f9d69eeaf189e16322e37
Arnelle Balane <arnellebalane@gmail.com>
2018-09-16 14:04:47 +0800

    Add initial LandingBanner definition

---
+++ src/components/LandingBanner.vue
@@ -0,0 +1,19 @@
+<template>
+  <section :class="$style.banner">
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LandingBanner',
+};
+</script>
+
+<style module="$shared" src="@/stylesheets/shared.css"></style>
+<style module>
+.banner {
+  height: 70rem;
+  border: 1px solid red;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="home">
     <TheNavbar />
+    <LandingBanner :class="$style.banner" />
   </div>
 </template>
 
 <script>
 import TheNavbar from '@/components/TheNavbar.vue';
+import LandingBanner from '@/components/LandingBanner.vue';
 
 export default {
   name: 'Home',
 
   components: {
     TheNavbar,
+    LandingBanner,
   },
 };
 </script>
+
+<style module>
+.banner {
+  margin-top: -18rem;
+}
+</style>
