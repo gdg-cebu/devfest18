b3dedc91a71bd4f3697d592998918f235e5cdff0
Arnelle Balane <arnellebalane@gmail.com>
2018-09-16 13:10:36 +0800

    Cleanup project files to my preferences

--- src/store.js
+++ src/store.js
@@ -3,14 +3,17 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const state = {};
+
+const getters = {};
+
+const mutations = {};
+
+const actions = {};
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state,
+  getters,
+  mutations,
+  actions,
 });
